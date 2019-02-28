db17fb468f562a259bbe761820a3c799de72979a
tytremblay <ty.tremblay@gmail.com>
2019-02-28 01:14:44 -0500

    forgot to delete things

--- new-seed/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './containers/App/App';
-import { store } from './containers/App/store';
-
-render(<App store={store} />, document.getElementById('root'));
